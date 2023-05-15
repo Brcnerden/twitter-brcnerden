373fcbb9826bfe04ea63c5e244a519c893c43f24
Brcnerden <brcnerden@gmail.com>
2023-05-15 21:27:16 +0300

    github

--- src/components/FeedPagePhoto/FeedPagePhoto.styled.js
+++ src/components/FeedPagePhoto/FeedPagePhoto.styled.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 const Banner = styled.div`
   max-width: 910px;
   width: 100%;
+
   > img {
     max-width: 100%;
     object-fit: cover;
@@ -13,9 +14,13 @@ const ProfileFoto = styled.div`
   max-width: 150px;
   width: 100%;
   height: 150px;
+  position: fixed;
   > img {
     max-width: 100%;
     object-fit: cover;
+    z-index: 1;
+    border: 1px solid gray;
+    border-radius: 50%;
     margin-top: -50px;
   }
 `;
